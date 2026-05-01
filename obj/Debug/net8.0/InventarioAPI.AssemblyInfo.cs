bdddd34b43c8e45530b84bb0382145a35301b6e3
fernandomira <hernandezmirandafernando4@gmail.com>
2026-04-30 20:03:37 -0600

    Dockerfile

--- obj/Debug/net8.0/InventarioAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventarioAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be5e9a3a669bd4e8e82c82ccb71097e12fb3336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2876f826f9d8d4584c3b3d1e5c9bf22360d29828")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
